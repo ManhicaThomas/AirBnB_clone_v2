d8972e371ed8e0c7e781821ed0321b630ff71492
ManhicaThomas <thomasmanchica@gmail.com>
2024-04-17 09:58:59 -0700

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""This module creates a Amenity class"""
+""" Amenity Module for HBNB project """
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    """Class for managing amenity objects"""
-
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents an amenity data set."""
+    __tablename__ = 'amenities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
